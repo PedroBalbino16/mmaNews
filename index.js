43ec2e75087d704f09953b39c8678c5880d2b37c
Pedro Balbino <t-pedro.balbino@cni.com.br>
2023-09-04 11:42:11 -0300

    files set to .json

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-const puppeteer = require("puppeteer")
-const fs = require("fs/promises")
+const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
 
 async function start() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await page.goto("https://www.espn.com.br/mma/")
-
-  //Catch the titles
-  const f = await page.$("[class='contentItem__title contentItem__title--hero contentItem__title--story']")
-  const text = await (await f.getProperty('textContent')).jsonValue()
-  const title = 'Titulo: ';
+  await page.goto("https://www.espn.com.br/mma/");
 
   const newsTitles = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll("[class='contentItem__title contentItem__title--story']")).map(x => x.textContent)
+    return Array.from(
+      document.querySelectorAll(
+        "[class='contentItem__title contentItem__title--story']"
+      )
+    ).map((x) => {
+      const title = 'title';
+      return {
+        [title]: x.textContent,
+      };
+    });
   });
-  
+
   const folderName = 'news'; // Nome da pasta
   await fs.mkdir(folderName, { recursive: true });
-  
+
   for (let i = 0; i < newsTitles.length; i++) {
     const newsTitle = newsTitles[i];
-    const fileName = `news_${i+1}.txt`;
+    const fileName = `news_${i + 1}.json`;
     const filePath = `${folderName}/${fileName}`;
-    const contentToAdd = `${title}${newsTitle}`
-    console.log('News', i+1, ': ', newsTitle)
 
-    fs.writeFile(filePath, contentToAdd)
+    const contentToAdd = JSON.stringify(newsTitle, null, 2); // Converte o objeto para JSON formatado
+
+    console.log('News', i + 1, ': ', newsTitle);
+
+    await fs.writeFile(filePath, contentToAdd);
   }
-  await browser.close()
+
+  await browser.close();
 }
 
-start()
+start();
