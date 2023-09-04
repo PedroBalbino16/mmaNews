6f3e801854694cef8973f303eccd00d074ef76be
Pedro Balbino <t-pedro.balbino@cni.com.br>
2023-09-04 11:47:14 -0300

    descriptions set to json

--- description.js
+++ description.js
@@ -10,18 +10,27 @@ async function start() {
   const f = await page.$("[class='contentItem__subhead contentItem__subhead--story']")
   const text = await (await f.getProperty('textContent')).jsonValue()
 
+
   const newsDescriptions = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll("[class='contentItem__subhead contentItem__subhead--story']")).map(x => x.textContent)
+    return Array.from(
+      document.querySelectorAll(
+        "[class='contentItem__subhead contentItem__subhead--story']"
+      )
+    ).map((x) => {
+      const description = 'description';
+      return {
+        [description]: x.textContent,
+      };
+    });
   });
-  const description = 'Descrição:\n';
   
   const folderName = 'news'; // Nome da pasta
 
   for (let i = 0; i < newsDescriptions.length; i++) {
     const newsDescription = newsDescriptions[i];
-    const fileName = `news_${i+1}.txt`;
+    const fileName = `news_${i+1}.json`;
     const filePath = `${folderName}/${fileName}`;
-    const contentToAdd = `\n\n${description}${newsDescription}`
+    const contentToAdd = JSON.stringify(newsDescription, null, 2);
     console.log('News', i+1, ': ', newsDescription)
 
     await fs.appendFile(filePath, contentToAdd)
