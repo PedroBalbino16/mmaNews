766233c7bc76b7232634f41c0c459255e800e9fb
Pedro Balbino <t-pedro.balbino@cni.com.br>
2023-09-11 10:42:47 -0300

    fix: json data and setting the code to a single run

---
+++ scrape.js
@@ -0,0 +1,49 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
+
+async function scrapeNewsTitlesAndDescriptions() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto("https://www.espn.com.br/mma/");
+
+  // Scrape news titles and descriptions
+  const newsData = await page.evaluate(() => {
+    const titles = Array.from(
+      document.querySelectorAll(
+        "[class='contentItem__title contentItem__title--story']"
+      )
+    ).map((x) => x.textContent);
+
+    const descriptions = Array.from(
+      document.querySelectorAll(
+        "[class='contentItem__subhead contentItem__subhead--story']"
+      )
+    ).map((x) => x.textContent);
+
+    return titles.map((title, index) => ({
+      title,
+      description: descriptions[index],
+    }));
+  });
+
+  // Create a folder for news data
+  const folderName = 'news_data';
+  await fs.mkdir(folderName, { recursive: true });
+
+  // Save news data as JSON files
+  for (let i = 0; i < newsData.length; i++) {
+    const newsItem = newsData[i];
+    const fileName = `news_${i + 1}.json`;
+    const filePath = `${folderName}/${fileName}`;
+    const contentToAdd = JSON.stringify(newsItem, null, 2);
+
+    console.log('News', i + 1, ': ', newsItem);
+
+    await fs.writeFile(filePath, contentToAdd);
+  }
+
+  await browser.close();
+}
+
+scrapeNewsTitlesAndDescriptions();
